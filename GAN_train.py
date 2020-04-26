8084331d2c204b9227eccc704452f04b91a8aec2
Rivenlalala <riven012303@gmail.com>
2020-04-25 20:29:24 -0400

    GAN

--- GAN_train.py
+++ GAN_train.py
@@ -12,7 +12,10 @@ recp_field = 1276
 sample_len = 4000 * 8
 
 netW = WaveNet(dilation_depth=8).cuda()
-netG = GenLSTM(stride=500).double().cuda()
+for param in netW.parameters():
+    param.requires_grad = False
+# netG = GenLSTM(stride=500).double().cuda()
+netG = nn.LSTMCell(256, 256).cuda()
 netD = DisLSTM().double().cuda()
 optimizerD = optim.Adam(netD.parameters(), lr=1e-3)
 optimizerG = optim.Adam(netG.parameters(), lr=1e-3)
@@ -32,6 +35,7 @@ def wavenetGen(batch_size=5, sample_len=4, recp_field=1276):
         if i >= batch_size:
             break
         seed = seed[:, :, 500:recp_field + 500].float().cuda()
+        # sample = Variable(sample.type(torch.FloatTensor)).cuda()
         output = seed
         # r_input = seed[:, :, -1]
         # r_input = r_input.unsqueeze(0).double()
@@ -41,33 +45,47 @@ def wavenetGen(batch_size=5, sample_len=4, recp_field=1276):
             new_mag = p.sample()
             new = new.zero_()
             new[:, new_mag] = 1
-            '''
             if index % 500 == 499:
-                r_new = netG(r_input)
-                r_new = r_new[:, -1, :]
-                r_new_mag = r_new.argmax(1)
-                r_new = r_new.zero_()
-                r_new[:, r_new_mag] = 1
-                r_input = torch.cat((r_input, new.permute(0, 2, 1).detach().double()), dim=1)
+                r_input = new.squeeze(2)
+                try:
+                    h, c = netG(r_input, (h, c))
+                except:
+                    h, c = netG(r_input)
+                    r_new = h
+                    r_new_mag = r_new.argmax(1)
+                    r_new = r_new.zero_()
+                    r_new[:, r_new_mag] = 1
+                    r_output = r_new.unsqueeze(0)
+                else:
+                    r_new = h
+                    r_new_mag = r_new.argmax(1)
+                    r_new = r_new.zero_()
+                    r_new[:, r_new_mag] = 1
+                # r_input = torch.cat((r_input, new.permute(0, 2, 1).detach().double()), dim=1)
+                    r_output = torch.cat((r_output, r_new.unsqueeze(0)), dim=1)
                 output = torch.cat((output, r_new.view(1, 256, 1).float()), dim=2)
             else:
-            '''
-            output = torch.cat((output, new.view(1, 256, 1)), dim=2)
+                output = torch.cat((output, new.view(1, 256, 1)), dim=2)
             seed = output[:, :, -recp_field:]
         if i == 0:
             sample = output
+            r_sample = r_output.unsqueeze(0)
         else:
             sample = torch.cat((sample, output), dim=0)
-    return sample
+            r_sample = torch.cat((r_sample, r_output.unsqueeze(0)), dim=0)
+        print(i)
+    return r_sample, sample
 
 
 for epoch in range(100):
     for i, (data, _, _) in enumerate(dataloader):
         # update D
         # real batch
+        print('updating D')
+        print('real batch')
         netD.zero_grad()
         data = data[:, :, 0:-1:500].cuda()
-        real = data.permute(0, 2, 1).double() # input: Tensor[batch, time_Step, in_depth]
+        real = data.permute(0, 2, 1).double()  # input: Tensor[batch, time_Step, in_depth]
         b_size = real.size(0)
         label = torch.full((b_size, 1), 1).double().cuda()
         output = netD(real)
@@ -75,9 +93,10 @@ for epoch in range(100):
         errD_real.backward()
 
         # fake batch
-        fake = wavenetGen()
+        print('fake batch')
+        print('generating')
+        fake, _ = wavenetGen()
         fake = fake.double()
-        fake = netG(fake)
         label.fill_(0)
         output = netD(fake.detach())
         errD_fake = criterion(output, label)
@@ -96,5 +115,3 @@ for epoch in range(100):
 
         print('[%d/500][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f'
               % (epoch, i, len(dataloader), errD.item(), errG.item()))
-
-
